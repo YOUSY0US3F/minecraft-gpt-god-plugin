f237d93a9e5a2cbdc56e46c011174865c11a0829
Yousef <yattia22@gmail.com>
2024-04-14 00:49:08 -0400

    fix: helmets

--- src/main/java/net/bigyous/gptgodmc/GPT/Prompts.java
+++ src/main/java/net/bigyous/gptgodmc/GPT/Prompts.java
@@ -6,7 +6,7 @@ import net.bigyous.gptgodmc.enums.GptGameMode;
 
 public class Prompts {
     private static Map<GptGameMode, String> Prompts = Map.ofEntries(
-        Map.entry(GptGameMode.SANDBOX, "You will roleplay as the god of a small minecraft island world, the players inhabiting the island have no resources. you will recieve information about whats has happened on the island."),
+        Map.entry(GptGameMode.SANDBOX, "You will roleplay as the god of a small minecraft island world, the players inhabiting the island have few resources, give the players challenges and reward them if they complete them. you will recieve information about whats has happened on the island."),
         Map.entry(GptGameMode.DEATHMATCH, "You will roleplay as the god of a small minecraft world, the players are split into two teams that must fight to the death. Each team spawns on their own floating island. You will decide which team to support based on which team aligns best with the reward / punish list.")
     );
 
--- src/main/java/net/bigyous/gptgodmc/RoundSystem.java
+++ src/main/java/net/bigyous/gptgodmc/RoundSystem.java
@@ -5,19 +5,25 @@ import java.util.Collections;
 import java.util.List;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Color;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.Server;
 import org.bukkit.block.Skull;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
+
+import com.destroystokyo.paper.event.player.PlayerPostRespawnEvent;
 
 import net.bigyous.gptgodmc.GPT.GptActions;
 import net.bigyous.gptgodmc.enums.GptGameMode;
@@ -92,6 +98,19 @@ public class RoundSystem implements Listener {
         if(GPTGOD.gameMode.equals(GptGameMode.DEATHMATCH)){
             addPlayerToTeam(event.getPlayer());
         }   
+    }
+
+    @EventHandler
+    public void onPlayerSpawn(PlayerPostRespawnEvent event){
+        if(GPTGOD.gameMode.equals(GptGameMode.DEATHMATCH)){
+            Player player = event.getPlayer();
+            ItemStack helm = new ItemStack(Material.LEATHER_HELMET);
+            LeatherArmorMeta helmMeta = (LeatherArmorMeta) helm.getItemMeta();
+            helmMeta.setColor(GPTGOD.SCOREBOARD.getEntityTeam(player).getName().equals("Red")? Color.RED : Color.BLUE);
+            helm.setItemMeta(helmMeta);
+            player.getInventory().setHelmet(helm);
+            player.getInventory().getHelmet().addEnchantment(Enchantment.BINDING_CURSE, 1);
+        }
     }
     @EventHandler
     public void onPlayerDisconnect(PlayerQuitEvent event) {
