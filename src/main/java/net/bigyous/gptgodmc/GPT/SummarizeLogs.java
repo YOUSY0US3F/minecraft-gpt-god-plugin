785fdfefd35033ec6a52cc2588ba29856ebe63dc
YOUSY0US3F <yattia22@gmail.com>
2024-05-17 18:44:18 +0300

    feat: upgraded to 4o

--- src/main/java/net/bigyous/gptgodmc/GPT/SummarizeLogs.java
+++ src/main/java/net/bigyous/gptgodmc/GPT/SummarizeLogs.java
@@ -19,7 +19,7 @@ public class SummarizeLogs {
         You are a helpful assistant that will recieve a log of events from a minecraft server, \
         or a historical summary and a log of events. \
         You will create a short summary based on this information that preserves the plot detailed by both, you are viewing these logs from the perspective of a god that rewards %s and punishes %s \
-        If information in the logs isn't important to the plot omit it. Do not add any extra flourishes, just state the facts, pay attention to actions that align with any objectives listed in the objectives.
+        Keep track of the reputation of each player, if information in the logs isn't important to the plot omit it. Do not add any extra flourishes, just state the facts, pay attention to actions that align with any objectives listed in the objectives.
         These logs are the history of the server so keep everything in the past tense.
         """;
     private static Gson gson = new Gson();
