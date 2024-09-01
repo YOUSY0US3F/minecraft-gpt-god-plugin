7f69bb9cc0685bcbfdad25a8b579d9ea2a973f01
Yousef <yattia22@gmail.com>
2024-09-01 12:12:22 -0400

    fix: 4o mini name

--- src/main/java/net/bigyous/gptgodmc/GPT/GPTModels.java
+++ src/main/java/net/bigyous/gptgodmc/GPT/GPTModels.java
@@ -9,7 +9,7 @@ import net.bigyous.gptgodmc.GPT.Json.GptModel;
 public class GPTModels {
     private static FileConfiguration config = JavaPlugin.getPlugin(GPTGOD.class).getConfig();
     public static final GptModel GPT_4o = new GptModel("gpt-4o", 100000);
-    public static final GptModel GPT_4o_mini = new GptModel("gpt-4o-turbo", 85000);
+    public static final GptModel GPT_4o_mini = new GptModel("gpt-4o-mini", 85000);
 
     public static GptModel getMainModel(){
         return config.getBoolean("use-gpt-4")? GPT_4o: GPT_4o_mini;
