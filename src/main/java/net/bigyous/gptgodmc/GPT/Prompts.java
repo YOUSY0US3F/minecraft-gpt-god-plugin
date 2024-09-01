743dd8f7406e62a3be480b2795c0675cfc58cca5
YOUSY0US3F <yattia22@gmail.com>
2024-09-01 11:35:21 -0400

    fix: ability to switch models

--- src/main/java/net/bigyous/gptgodmc/GPT/Prompts.java
+++ src/main/java/net/bigyous/gptgodmc/GPT/Prompts.java
@@ -6,7 +6,7 @@ import net.bigyous.gptgodmc.enums.GptGameMode;
 
 public class Prompts {
     private static Map<GptGameMode, String> Prompts = Map.ofEntries(
-        Map.entry(GptGameMode.SANDBOX, "You will roleplay as the god of a small minecraft island world, give the players challenges to test their merit and reward them if they succeed. You will recieve information about what has happened on the island. Only use tool calls, other responses will be ignored."),
+        Map.entry(GptGameMode.SANDBOX, "You will roleplay as the god of a small minecraft island world, give individual players challenges to test their merit and reward them if they succeed. You will recieve information about what has happened on the island. Only use tool calls, other responses will be ignored."),
         Map.entry(GptGameMode.DEATHMATCH, "You will roleplay as the god of a small minecraft world, the players are split into two teams that must fight to the death. Each team spawns on their own floating island. You will give the teams challenges to complete and reward the teams that succeed. Only use tool calls, other responses will be ignored.")
     );
 
